c9ac6ca699a862bf8bab09ce2957f35a2db5b335
Sohail Ansari <sohail.ansari@expeditors.com>
2024-10-24 16:21:04 +0530

    Base_Project

---
+++ src/test/java/features/login.feature
@@ -0,0 +1,8 @@
+Feature: Validating Login Functionalities of the UAM Application
+
+  Scenario: Valid Login Functionality
+
+    Given Launch the Url in the chrome browser
+    When Enter the email "" in the email field
+    When Enter the password "" in the password field
+    Then Click the Login Button in the Login screen
---
+++ src/test/java/framework/TestRunner.java
@@ -0,0 +1,19 @@
+package framework;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+		monochrome = true,
+		features = "src/test/java/features",
+		glue = {"logic","framework"},
+		plugin = {"pretty","json:target/cucumber.json", "html:target/cucumber-html-report"},
+		tags = {"@test"}
+		)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
